06274a4f2ed578edc783bc2fe3690920a203918c
f1781600921 <1781600921@qq.com>
2023-11-29 14:41:50 +0800

    re

--- omni_IHU519G.mk
+++ omni_IHU519G.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -20,4 +20,10 @@ PRODUCT_NAME := omni_IHU519G
 PRODUCT_BRAND := ecarx
 PRODUCT_MODEL := FS11A1
 PRODUCT_MANUFACTURER := ecarx
-PRODUCT_RELEASE_NAME := ecarx
+
+PRODUCT_GMS_CLIENTID_BASE := android-ecarx
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="IHU519G-user 9 PQ2A.190405.003 293 dev-keys"
+
+BUILD_FINGERPRINT := ecarx/IHU519G/IHU519G:9/PQ2A.190405.003/293:user/dev-keys
