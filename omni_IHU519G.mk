74e6890a97b1a9a7daa3daa17afd79a05dd62556
f1781600921 <49328623+f1781600921@users.noreply.github.com>
2023-12-02 14:52:44 +0800

    Update omni_IHU519G.mk

--- omni_IHU519G.mk
+++ omni_IHU519G.mk
@@ -15,8 +15,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from IHU519G device
 $(call inherit-product, device/ecarx/IHU519G/device.mk)
 
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := IHU519G
 PRODUCT_NAME := omni_IHU519G
