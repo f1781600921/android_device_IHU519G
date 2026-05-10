2d1398674a2af6c712d96d5a674b66ea18b3aab1
f1781600921 <49328623+f1781600921@users.noreply.github.com>
2026-05-10 15:36:58 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,8 @@
 add_lunch_combo omni_IHU519G-user
 add_lunch_combo omni_IHU519G-userdebug
 add_lunch_combo omni_IHU519G-eng
+
+export FOX_BUILD_DEVICE=IHU519G
+export OF_MAINTAINER=Ashuai
+export FOX_VANILLA_BUILD=1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
